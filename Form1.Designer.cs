ef2b29ae40d5ea44116e2a097cf39dc249aec288
opilane <opilane@HP-CZC6297B8Z>
2021-12-08 15:35:41 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace kamennozbumaga
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Rock = new System.Windows.Forms.Button();
+            this.Paper = new System.Windows.Forms.Button();
+            this.Scissors = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Rock
+            // 
+            this.Rock.Location = new System.Drawing.Point(56, 118);
+            this.Rock.Name = "Rock";
+            this.Rock.Size = new System.Drawing.Size(75, 23);
+            this.Rock.TabIndex = 0;
+            this.Rock.Text = "Rock";
+            this.Rock.UseVisualStyleBackColor = true;
+            // 
+            // Paper
+            // 
+            this.Paper.Location = new System.Drawing.Point(56, 160);
+            this.Paper.Name = "Paper";
+            this.Paper.Size = new System.Drawing.Size(75, 23);
+            this.Paper.TabIndex = 1;
+            this.Paper.Text = "Paper";
+            this.Paper.UseVisualStyleBackColor = true;
+            // 
+            // Scissors
+            // 
+            this.Scissors.Location = new System.Drawing.Point(56, 202);
+            this.Scissors.Name = "Scissors";
+            this.Scissors.Size = new System.Drawing.Size(75, 23);
+            this.Scissors.TabIndex = 2;
+            this.Scissors.Text = "Scissors";
+            this.Scissors.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(207, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(147, 135);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(524, 48);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(147, 135);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(56, 334);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label1.Location = new System.Drawing.Point(249, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(578, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label3.Location = new System.Drawing.Point(344, 290);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label4.Location = new System.Drawing.Point(500, 290);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Scissors);
+            this.Controls.Add(this.Paper);
+            this.Controls.Add(this.Rock);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Rock;
+        private System.Windows.Forms.Button Paper;
+        private System.Windows.Forms.Button Scissors;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kamennozbumaga
+{
+    public partial class Form1 : Form
+    {
+        
+        string Computer;
+        string[] computer = { "Rock", "Paper", "Scissors" };
+        Random random = new Random();
+        int RandomType;
+        string PlayerPicks;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StoneButton_Click(object sender, EventArgs e)
+        {
+            PlayerPicks = "Rock";
+            RandomType = random.Next(0, 3);
+            Computer = computer[RandomType];
+            Game();
+        }
+
+        private void PaperButton_Click(object sender, EventArgs e)
+        {
+            PlayerPicks = "Paper";
+            RandomType = random.Next(0, 3);
+            Computer = computer[RandomType];
+            Game();
+        }
+
+        private void ScissorsButton_Click(object sender, EventArgs e)
+        {
+            PlayerPicks = "Scissors";
+            RandomType = random.Next(0, 3);
+            Computer = computer[RandomType];
+            Game();
+        }
+
+        void Game()
+        {
+            string message = "Võitja on: ";
+            string computerWins = "Arvuti!";
+            string playerWins = "Mängija!";
+            string draw = "Mitte keegi?";
+
+
+            if (PlayerPicks == "Rock" && Computer == "Paper") // mängija: kivi, Arvuti: paber = Arvuti võidab
+            {
+                MessageBox.Show(message + computerWins);
+            }
+            else if (PlayerPicks == "Rock" && Computer == "Scissors") // mängija: kivi, Arvuti: Käärid = Arvuti võidab
+            {
+                MessageBox.Show(message + playerWins);
+
+            }
+            else if (PlayerPicks == "Paper" && Computer == "Scissors") // mängija: PAber, Arvuti: Käärid = Arvuti võidab
+            {
+                MessageBox.Show(message + computerWins);
+
+            }
+            else if (PlayerPicks == "Paper" && Computer == "Rock") // mängija: Paber, Arvuti: kivi = mängija võidab
+            {
+                MessageBox.Show(message + playerWins);
+
+            }
+            else if (PlayerPicks == "Scissors" && Computer == "Rock") // mängija: Käärid, Arvuti: kivi = Arvuti võidab
+            {
+                MessageBox.Show(message + computerWins);
+
+            }
+            else if (PlayerPicks == "Scissors" && Computer == "Paper") // mängija: Käärid, Arvuti: Paber = Maängija võidab
+            {
+                MessageBox.Show(message + playerWins);
+
+            }
+            if (PlayerPicks == "Scissors" && Computer == "Scissors") // mängija: Käärid, Arvuti Scissors = Viik
+            {
+                MessageBox.Show(message + draw);
+
+            }
+            if (PlayerPicks == "Paper" && Computer == "Paper")// mängija: Käärid, Arvuti Scissors = Viik
+            {
+                MessageBox.Show(message + draw);
+            }
+            if (PlayerPicks == "Rock" && Computer == "Rock")
+            {
+                MessageBox.Show(message + draw);
+            }
+            if (PlayerPicks == "Scissros" && Computer == "Scissors")// mängija: Käärid, Arvuti Scissors = Viik
+            {
+                MessageBox.Show(message + draw);
+            }
+            private void playerswWins_Click(object sender, EventArgs e)
+            {
+                if (PlayerPicks > Computer)
+                {
+                    PlayerPicks = Computer + " Wins the game";
+                }
+                else
+                {
+                    PlayerPicks = "Win";
+                }
+            }
+        }
+
+       
+    }
+}
